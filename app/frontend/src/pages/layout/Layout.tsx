e5f4db91f3decf80911eec100618f9dcd4fbce4d
kostas <kostas@mantodea.io>
2023-11-07 18:04:10 +0100

    changed text in the chat page

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -5,6 +5,7 @@ import github from "../../assets/github.svg";
 import styles from "./Layout.module.css";
 
 import { useLogin } from "../../authConfig";
+import { useLogin } from "../../authConfig";
 
 import { LoginButton } from "../../components/LoginButton";
 
@@ -14,7 +15,7 @@ const Layout = () => {
             <header className={styles.header} role={"banner"}>
                 <div className={styles.headerContainer}>
                     <Link to="/" className={styles.headerTitleContainer}>
-                        <h3 className={styles.headerTitle}>GPT + Enterprise data | Sample</h3>
+                        <h3 className={styles.headerTitle}>Dental GPT</h3>
                     </Link>
                     <nav>
                         <ul className={styles.headerNavList}>
@@ -28,21 +29,9 @@ const Layout = () => {
                                     Ask a question
                                 </NavLink>
                             </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
-                                    <img
-                                        src={github}
-                                        alt="Github logo"
-                                        aria-label="Link to github repository"
-                                        width="20px"
-                                        height="20px"
-                                        className={styles.githubLogo}
-                                    />
-                                </a>
-                            </li>
                         </ul>
                     </nav>
-                    <h4 className={styles.headerRightText}>Azure OpenAI + AI Search</h4>
+                    <h4 className={styles.headerRightText}>Dental Market Insights</h4>
                     {useLogin && <LoginButton />}
                 </div>
             </header>
